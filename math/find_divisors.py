bcc7591cdc239e808cb69266b558dcff8171bb10
Glory <dmb07223@jinhakapply.com>
2025-01-07 16:31:41 +0900

    Create find_divisors.py

---
+++ math/find_divisors.py
@@ -0,0 +1,16 @@
+def find_divisors(n):
+    divisors = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            divisors.append(i)
+    return divisors
+
+def find_divisors_square_root(n):
+    divisors = []
+    
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i: # n이 4일 경우 2를 2번 넣지 않게 방지
+                divisors.append(n // i)
+    return sorted(divisors)
