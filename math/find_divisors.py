bdca8dd20a9eaae2cee6379b3163b3c1edeb62d2
Glory <dmb07223@jinhakapply.com>
2025-01-17 12:16:28 +0900

    add comment

--- math/find_divisors.py
+++ math/find_divisors.py
@@ -1,5 +1,6 @@
 # 약수 구하기
 
+# 전체 확인
 def find_divisors(n):
     divisors = []
     for i in range(1, n + 1):
@@ -7,6 +8,7 @@ def find_divisors(n):
             divisors.append(i)
     return divisors
 
+# 절반 잘라 확인
 def find_divisors_square_root(n):
     divisors = []
     
